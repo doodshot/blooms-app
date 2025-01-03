c7dad9d0d62e6ba09def09ea9f22b59711adc1e1
doodshot <guido.panzieri@edu.itspiemonte.it>
2025-01-03 19:09:03 +0100

    fix risolto bug del tab navigator sul suo background

--- src/ui/screens/HomePage.tsx
+++ src/ui/screens/HomePage.tsx
@@ -1,9 +1,25 @@
-import {View, Text} from "react-native";
+import {View, StyleSheet, SafeAreaView} from "react-native";
 
+
+// la pagina del gioco per fare i punti
 export default function HomePage() {
     return (
-        <View>
-            <Text>HomePage</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#163440',
+    },
+    content: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
