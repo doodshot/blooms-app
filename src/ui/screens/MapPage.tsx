a0ff8f69df0cbed5491d4214e367b9e0d1de62eb
doodshot <guido.panzieri@edu.itspiemonte.it>
2025-01-15 15:23:24 +0100

    fix aggiunto mappa

--- src/ui/screens/MapPage.tsx
+++ src/ui/screens/MapPage.tsx
@@ -1,18 +1,44 @@
-import { StyleSheet, View } from 'react-native';
+import {Image, SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
 export default function MapPage() {
     return (
-       <View style={styles.container}>
-
-       </View>
+     <SafeAreaView style={styles.container}>
+         <Ionicons name={"rose-outline"} size={64} color={"#163440"} style={styles.roseIcon} />
+         <View style={styles.title}>
+             <Text style={styles.titleTxt}>
+                 Ecco la Mappa!
+             </Text>
+         </View>
+         <Image
+             style={styles.img}
+             source={require("../../../assets/Mappa.png")}>
+         </Image>
+     </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#",
+        backgroundColor: "#A1BCC1",
     },
-    glView: {
-        flex: 1,
+    img: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain",
+        marginRight: 16
     },
+    title: {
+        marginHorizontal: 16,
+    },
+    titleTxt: {
+        fontFamily: "Poppins-SemiBold",
+        fontSize: 24,
+        color: "#163440",
+    },
+    roseIcon: {
+        position: 'absolute',
+        right: 20,
+        top: 42,
+    }
 });
